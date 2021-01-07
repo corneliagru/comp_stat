c259086ef246cdee756f28a76ee606c0d6943ddd
Cornelia Gruber <cornelia.gruber@campus.lmu.de>
2021-01-07 13:07:38 +0100

    committ

--- assignment_gruber.R
+++ assignment_gruber.R
@@ -1,19 +1,94 @@
-#data import
 
-source('preparation.R')
+# import ------------------------------------------------------------------
+
 
 data <- read.table("data.txt")
 x <- data$V1
 
-hist(data$V1, breaks = 30)
+source("preparation.R")
 
 
-plot(density(data$V1))
-lines(x, 1*f_x_giv_delta(x, 2.8), col = "red")
+# description -------------------------------------------------------------
+
+round(summary(x), digits = 2 )
 
 
-x = seq(0,8, by = 0.1)
-plot(x, 0.7*f_x_giv_delta(x, 2.8) + 0.3*f_x_giv_delta(x, 5), type = "l")
-lines(x, 1*f_x_giv_delta(x, 2.8), col = "red")
-lines(x, 0.3*f_x_giv_delta(x, 5), col = "red")
 
+
+# find best parameter settings. apparently there is only one group, all deltas
+# are basically the same
+param <- EM(x, k = 5)
+param$delta
+
+# only one class
+param <- EM(x, k = 1)
+delta <- param$delta
+p <- param$p
+
+
+xx = seq(0,8, by = 0.1)
+yy <- sapply(xx, dens_plot, delta = delta, p = p)
+
+plot(density(x), ylim = c(0,0.4), main = "")
+lines(xx, yy, col = "red")
+legend("topright", legend=c("observed data", "delta = 2.37"),
+       col=c("black", "red"),lty = 1,  cex=1.1)
+
+
+
+
+
+
+res <- MCMC(theta_0 = 1, delta = delta, p = p, n = 15000)
+
+acf(res, lag.max = 50)
+# -> n = 2000, lag 35
+plot(ts(res))
+# -> n = 2000, burn in 100
+
+samp <- MCMC_iid(res, 40, 100)
+
+
+plot(density(samp))
+lines(density(x), col = "red")
+
+
+res_bs <- bs_param(B = 10, delta = delta, p = p)
+
+apply(res_bs, 2, quantile, probs = c(0.025, 0.975))
+
+
+
+#prepare
+param <- EM(x, k = 1)
+delta <- param$delta
+p <- param$p
+
+#running on server/cluster so I can use all 40 cores
+numCores <- detectCores()
+registerDoParallel(numCores)
+source("preparation.R")
+start <- Sys.time()
+res_bs <- bs_param(B = 1000, delta = delta, p = p, parallel = TRUE)
+end <- Sys.time()
+print(end - start)
+stopImplicitCluster()
+
+
+
+
+
+ci <- apply(res_bs, 2, quantile, probs = c(0.025, 0.975))
+d_lower <- ci[1,1]
+d_upper <- ci[2,1]
+
+xx = seq(0,8, by = 0.1)
+yy <- sapply(xx, dens_plot, delta = d_lower, p = p)
+yyy <- sapply(xx, dens_plot, delta = d_upper, p = p)
+
+
+plot(density(x), ylim = c(0,0.4), main = "")
+lines(xx, yy, col = "gray20", lty = "dotted")
+lines(xx, yyy, col = "gray20", lty = "longdash")
+legend("topright", legend=c("delta = 2.19", "delta = 2.59"),
+       col=c("gray20", "gray20"), lty=c("dotted", "longdash"), cex=1.1)
--- preparation.R
+++ preparation.R
@@ -1,13 +1,21 @@
+library(doParallel)
+library(foreach)
 
-
+# density function
 dens <- function(x, delta) {
-  if (x < 0) return(0 * delta)  #so dimension fit
+  if (x < 0) {
+    return(0 * delta)
+  } # so dimension fit
   (delta / sqrt(2 * pi)) * exp(delta) * (x^(-1.5)) *
     exp((-0.5) * (((delta^2) / x) + x))
 }
 
+#for plotting
+dens_plot <- function(x, delta, p) dens(x, delta) %*% t(p)
 
 
+
+# EM algorithm to find best delta
 EM <- function(x, k = 2, initial = 1, sta = seq(min(x):max(x)),
                tolerance = 0.000000001, silent = TRUE) {
   n <- length(x)
@@ -23,7 +31,7 @@ EM <- function(x, k = 2, initial = 1, sta = seq(min(x):max(x)),
   w <- matrix(NA, n, k)
   # initialize result matrix for 5000 steps
   # TODO smarter way to inizialize size
-  result <- matrix(NA, 20000, (2 * k + 2))
+  result <- matrix(NA, 5000, (2 * k + 2))
   liknew <- 0
   iter <- 0
   finished <- 0
@@ -33,7 +41,7 @@ EM <- function(x, k = 2, initial = 1, sta = seq(min(x):max(x)),
     lik <- 0
     # E Step
     for (i in 1:n) {
-      #todo is hier ergebnis vektor?
+      # todo is hier ergebnis vektor?
       temp <- dens(x[i], delta) * p
       w[i, ] <- temp / sum(temp)
       lik <- lik + log(sum(temp))
@@ -44,6 +52,8 @@ EM <- function(x, k = 2, initial = 1, sta = seq(min(x):max(x)),
     if (criterion < tolerance) finished <- 1
     liknew <- lik
     if (!silent) print(c(iter, p, delta, lik))
+    # add another 5000 rows if result matrix not enough
+    if (iter == nrow(result)) result <- rbind(result, matrix(NA, 5000, (2 * k + 2)))
     result[iter, ] <- c(p, delta, lik, criterion)
   }
   list(
@@ -53,7 +63,7 @@ EM <- function(x, k = 2, initial = 1, sta = seq(min(x):max(x)),
 }
 
 
-
+# non parametric bootstrap for delta conf. intervals
 bs_non_param <- function(x, k = 2, initial = 1, sta = seq(min(x):max(x)),
                          tolerance = 0.000000001, seed = 1234, B = 1000, alpha = 0.05, silent = TRUE) {
   set.seed(seed)
@@ -61,23 +71,24 @@ bs_non_param <- function(x, k = 2, initial = 1, sta = seq(min(x):max(x)),
   n <- length(x)
   for (i in 1:B) {
     x_bs <- sample(x, n, replace = TRUE)
-    temp <- EM(x_bs, k = k, initial = initial, sta = sta,
-               tolerance = tolerance, silent = silent)
+    temp <- EM(x_bs,
+      k = k, initial = initial, sta = sta,
+      tolerance = tolerance, silent = silent
+    )
     res[i, ] <- temp$delta
   }
   apply(res, 2, quantile, probs = c(alpha / 2, 1 - alpha / 2))
-  
 }
 
 
-
+# sample values in a markov chain
 MCMC <- function(theta_0, delta, p, n) {
   res <- matrix(NA, n, 1)
   theta <- theta_0
-  
+
   for (i in 1:n) {
     theta_i <- rnorm(1, theta, 1)
-    alpha <- min(1, (p%*%t(dens(theta_i, delta)) / p%*%t(dens(theta, delta))))
+    alpha <- min(1, (p %*% t(dens(theta_i, delta)) / p %*% t(dens(theta, delta))))
     if (rbinom(1, 1, prob = alpha) == 1) theta <- theta_i
     res[i, ] <- theta
   }
@@ -85,27 +96,37 @@ MCMC <- function(theta_0, delta, p, n) {
 }
 
 
-
-MCMC_iid <- function(res, lag = 35, burn_in = 100) {
-  n <- nrow(res)
-  keep <- res[burn_in:n, ][seq(1, n-burn_in , by = lag)]
+# get iid samples from markov chain
+MCMC_iid <- function(res, n = 300, lag = 35, burn_in = 100) {
+  len <- nrow(res)
+  keep <- res[burn_in:len, ][seq(1, len - burn_in, by = lag)]
+  keep <- keep[1:n]
   keep
 }
 
 
-
-bs_param <- function(B = 100, delta, p, theta_0 = 1, n = 15000, lag = 40, burn_in = 100){
+# bootstrap CI from MCMC_iid
+bs_param <- function(B = 100, delta, p, theta_0 = 1, n = 15000, lag = 40, burn_in = 100, parallel = TRUE) {
   k <- length(delta)
-  result <- matrix(NA, B, 2*k)
-  for (i in 1:B) {
-    res <- MCMC(theta_0 = theta_0, delta = delta, p = p, n = n)
-    samp <- MCMC_iid(res, lag = lag, burn_in = burn_in)
-    param <- EM(samp, k = k)
-    result[i,] <- cbind(param$delta, param$p)
+  result <- matrix(NA, B, 2 * k)
+  if(parallel) {
+    foreach (i=1:B, .combine=rbind) %dopar% {
+      source("preparation.R")
+      res <- MCMC(theta_0 = theta_0, delta = delta, p = p, n = n)
+      samp <- MCMC_iid(res, lag = lag, burn_in = burn_in)
+      param <- EM(samp, k = k)
+      cbind(param$delta, param$p)
+    }
+  } else {
+    for (i in 1:B) {
+      res <- MCMC(theta_0 = theta_0, delta = delta, p = p, n = n)
+      samp <- MCMC_iid(res, lag = lag, burn_in = burn_in)
+      param <- EM(samp, k = k)
+      result[i, ] <- cbind(param$delta, param$p)
+      result
+    }
   }
-  result
 }
-
 
 
 
@@ -114,9 +135,8 @@ bs_param <- function(B = 100, delta, p, theta_0 = 1, n = 15000, lag = 40, burn_i
 
 
 
-B = 100
-theta_0 = 1
-n = 15000
-lag = 40
-burn_in = 100
-
+B <- 100
+theta_0 <- 1
+n <- 15000
+lag <- 40
+burn_in <- 100
